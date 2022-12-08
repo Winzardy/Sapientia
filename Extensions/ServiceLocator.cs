ff3f020adf6ae09ea9b20bcb710632bae7f8d572
Vlod Shvetsov <nsgbcz@gmail.com>
2022-12-08 11:23:34 +0300

    Add ServiceLocator

---
+++ Extensions/ServiceLocator.cs
@@ -0,0 +1,37 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Sapentia.Extensions
+{
+	public static class ServiceLocator<T> where T: IService
+	{
+		public static T Instance { [MethodImpl(MethodImplOptions.AggressiveInlining)] get; [MethodImpl(MethodImplOptions.AggressiveInlining)] private set; }
+
+		public static void Register(T service)
+		{
+			Debug.Assert(Instance == null);
+			Instance = service;
+		}
+
+		public static void UnRegister(T service)
+		{
+			Debug.Assert(Instance.Equals(service));
+			Instance = default;
+		}
+	}
+
+	public static class ServiceLocator
+	{
+		public static void RegisterAsService<T>(this T service) where T: IService
+		{
+			ServiceLocator<T>.Register(service);
+		}
+
+		public static void UnRegisterAsService<T>(this T service) where T: IService
+		{
+			ServiceLocator<T>.UnRegister(service);
+		}
+	}
+
+	public interface IService {}
+}
