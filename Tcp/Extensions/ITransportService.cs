108bbec47849e3af86fd742170d88ab9d8157726
Vlod Shvetsov <nsgbcz@gmail.com>
2022-12-09 12:46:28 +0300

    Update namespace in ITransportService

--- Tcp/Extensions/ITransportService.cs
+++ Tcp/Extensions/ITransportService.cs
@@ -3,16 +3,17 @@ using Sapientia.Extensions;
 using Sapientia.Transport;
 using Sapientia.Transport.RemoteMessage;
 
-namespace Sapientia.Tcp.Extensions;
-
-public interface ITransportService : IService
+namespace Sapientia.Tcp.Extensions
 {
-	public event Action<ConnectionReference> ConnectionReceivedEvent;
-	public event Action<RemoteMessage> MessageReceivedEvent;
+	public interface ITransportService : IService
+	{
+		public event Action<ConnectionReference> ConnectionReceivedEvent;
+		public event Action<RemoteMessage> MessageReceivedEvent;
 
-	public RemoteMessageSender GetMessageSender();
+		public RemoteMessageSender GetMessageSender();
 
-	public void Run();
+		public void Run();
 
-	public void ManuallyUpdate();
+		public void ManuallyUpdate();
+	}
 }
