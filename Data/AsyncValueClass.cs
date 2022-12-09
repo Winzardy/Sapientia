37d9f9c61c681d43d63d6760ae60d7c5a639f9fb
Vlod Shvetsov <nsgbcz@gmail.com>
2022-12-09 12:51:23 +0300

    Add AsyncValueClass

---
+++ Data/AsyncValueClass.cs
@@ -0,0 +1,73 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Sapientia.Extensions;
+
+namespace Sapientia.Data
+{
+	public class AsyncValueClass<T>
+	{
+		public T value;
+
+		private int _millisecondsTimeout;
+		private int _state;
+
+		public State State => _state.ToEnum<State>();
+
+		public AsyncValueClass(T value, int millisecondsTimeout = 1)
+		{
+			this.value = value;
+			_millisecondsTimeout = millisecondsTimeout;
+			_state = (int)State.Free;
+		}
+
+		public void SetMillisecondsTimeout(int millisecondsTimeout)
+		{
+			SetBusy();
+			_millisecondsTimeout = millisecondsTimeout;
+			SetFree();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool TrySetBusy()
+		{
+			return _state.Interlocked_CompareExchangeIntEnum(State.Busy, State.Free) == State.Free;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void SetBusy()
+		{
+			while (_state.Interlocked_CompareExchangeIntEnum(State.Busy, State.Free) == State.Busy)
+			{
+				Thread.Sleep(_millisecondsTimeout);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void SetFree()
+		{
+			_state = (int)State.Free;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public AsyncValueClassScope<T> GetScope()
+		{
+			return new AsyncValueClassScope<T>(this);
+		}
+	}
+
+	public readonly ref struct AsyncValueClassScope<T>
+	{
+		private readonly AsyncValueClass<T> _asyncValue;
+
+		public AsyncValueClassScope(AsyncValueClass<T> asyncValue)
+		{
+			asyncValue.SetBusy();
+			_asyncValue = asyncValue;
+		}
+
+		public void Dispose()
+		{
+			_asyncValue.SetFree();
+		}
+	}
+}
