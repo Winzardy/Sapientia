e53553369a956a848501580ddf41c34c8c4b5d92
Vlod Shvetsov <nsgbcz@gmail.com>
2023-01-27 00:28:09 +0300

    Add StreamExtensions

---
+++ Extensions/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Sapientia.Extensions;
+
+public static class StreamExtensions
+{
+	public static async Task<string> ReadToEndAsync(this Stream stream)
+	{
+		using var reader = new StreamReader(stream);
+		var result = await reader.ReadToEndAsync();
+		return result;
+	}
+}
