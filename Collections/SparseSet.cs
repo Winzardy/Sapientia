1c2655345b49fafe1f48c85ff77abd1442abc3eb
Vlod Shvetsov <nsgbcz@gmail.com>
2023-03-01 00:04:26 +0300

    Fix SparseSet

--- Collections/SparseSet.cs
+++ Collections/SparseSet.cs
@@ -7,10 +7,16 @@ namespace Sapientia.Collections
 {
 	public class SparseSet<T> : IDisposable
 	{
+		public delegate void ResetAction(ref T item);
+
 		public readonly int expandStep;
 
 		private T[] _values;
 		private int[] _valueIndexes;
+
+		private readonly bool _useIndexPool;
+		private readonly bool _useValuePool;
+		private readonly ResetAction _resetValue;
 
 		private int _count;
 		private int _capacity;
@@ -32,17 +38,22 @@ namespace Sapientia.Collections
 			get => _count >= _capacity;
 		}
 
-		public SparseSet(int capacity) : this(capacity, capacity) {}
+		public SparseSet(int capacity, ResetAction resetValue = null) : this(capacity, capacity, true, true, resetValue) {}
 
-		public SparseSet(int capacity, int expandStep)
+		public SparseSet(int capacity, int expandStep, bool useIndexPool = true, bool useValuePool = true, ResetAction resetValue = null)
 		{
 			this.expandStep = expandStep;
 
 			_count = 0;
 			_capacity = capacity;
 
-			_values = ArrayPool<T>.Shared.Rent(capacity);
-			_valueIndexes = ArrayPool<int>.Shared.Rent(capacity);
+			_useIndexPool = useIndexPool;
+			_useValuePool = useValuePool;
+			if (_useValuePool)
+				_resetValue = resetValue;
+
+			_valueIndexes = _useIndexPool ? ArrayPool<int>.Shared.Rent(capacity) : new int[capacity];
+			_values = _useValuePool ? ArrayPool<T>.Shared.Rent(capacity) : new T[capacity];
 
 			Fill(0, capacity);
 		}
@@ -86,8 +97,14 @@ namespace Sapientia.Collections
 
 		private void Expand(int newCapacity)
 		{
-			ArrayExtensions.Expand_WithPool(ref _values, newCapacity);
-			ArrayExtensions.Expand_WithPool(ref _valueIndexes, newCapacity);
+			if (_useIndexPool)
+				ArrayExtensions.Expand_WithPool(ref _valueIndexes, newCapacity);
+			else
+				ArrayExtensions.Expand(ref _valueIndexes, newCapacity);
+			if (_useValuePool)
+				ArrayExtensions.Expand_WithPool(ref _values, newCapacity);
+			else
+				ArrayExtensions.Expand(ref _values, newCapacity);
 
 			Fill(_capacity, newCapacity);
 
@@ -105,11 +122,28 @@ namespace Sapientia.Collections
 			{
 				_valueIndexes[i] = i;
 			}
+			if (_resetValue == null)
+				return;
+			for (var i = from; i < to; i++)
+			{
+				_resetValue.Invoke(ref _values[i]);
+			}
 		}
 
 		public void Dispose()
 		{
-			ArrayPool<T>.Shared.Return(_values);
+			Dispose(false);
+		}
+
+		public void Dispose(bool clearArray)
+		{
+			if (_useIndexPool)
+				ArrayPool<int>.Shared.Return(_valueIndexes, clearArray);
+			if (_useValuePool)
+				ArrayPool<T>.Shared.Return(_values, clearArray);
+
+			_valueIndexes = null;
+			_values = null;
 		}
 	}
 }
