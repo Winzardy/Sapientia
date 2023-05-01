285fb58cb9b15c23000f5686fe64a354e5605093
Vlod Shvetsov <nsgbcz@gmail.com>
2023-05-01 12:19:24 +0300

    Add EnvironmentExtensions

---
+++ Extensions/EnvironmentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Sapientia.Extensions
+{
+	public static class EnvironmentExtensions
+	{
+		public static string ReadEnvironment(this string environment)
+		{
+			var value = Environment.GetEnvironmentVariable(environment);
+			if (value == null)
+				throw new Exception($"{environment} doesn't exist!");
+			return value;
+		}
+
+		public static string ReadEnvironmentFile(this string environment)
+		{
+			var path = ReadEnvironment(environment);
+			if (!File.Exists(path))
+				throw new Exception($"{environment} contains incorrect path!");
+			return File.ReadAllText(path);
+		}
+	}
+}
