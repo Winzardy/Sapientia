7ab50144e645d8bb6f1e659972b8b1530c0ca376
Vlod Shvetsov <nsgbcz@gmail.com>
2023-06-21 14:30:24 +0300

    StreamExtensions add ReadLinesAsync

--- Extensions/StreamExtensions.cs
+++ Extensions/StreamExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Sapientia.Collections;
 
 namespace Sapientia.Extensions
 {
@@ -10,6 +12,30 @@ namespace Sapientia.Extensions
 			using var reader = new StreamReader(stream);
 			var result = await reader.ReadToEndAsync();
 			return result;
+		}
+
+		public static async Task<SimpleList<string>?> ReadLinesAsync(this Stream stream)
+		{
+			using var reader = new StreamReader(stream);
+			if (reader.Peek() == 0)
+				return default;
+
+			var result = new SimpleList<string>();
+
+			try
+			{
+				do
+				{
+					var value = (await reader.ReadLineAsync())!;
+					result.Add(value);
+				} while (reader.Peek() > 0);
+
+				return result;
+			}
+			catch (Exception e)
+			{
+				return default;
+			}
 		}
 	}
 }
