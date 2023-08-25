85a87ea921478ecd44b3f8df7896db3fe29e2f65
Vlod Shvetsov <nsgbcz@gmail.com>
2023-08-25 16:57:19 +0300

    Update UnsafeExtensions

--- Extensions/UnsafeExtensions.cs
+++ Extensions/UnsafeExtensions.cs
@@ -27,52 +27,25 @@ namespace Sapientia.Extensions
 #endif
 		}
 
-		public static bool IsEquals<T>(this ref T a, ref T b) where T : unmanaged
+		public static bool IsEquals<T>(this ref T a, ref T b) where T : struct
 		{
-			var size = sizeof(T);
-
-			var bytes = size % sizeof(int);
-			var ints = (size - bytes) / sizeof(int);
-
-			var intPtrA = (int*)AsPointer(ref a);
-			var intPtrB = (int*)AsPointer(ref b);
-			for (var i = 0; i < ints; i++)
-			{
-				if (intPtrA[i] != intPtrB[i])
-					return false;
-			}
-			var bytePtrA = (byte*)intPtrA;
-			var bytePtrB = (byte*)intPtrB;
-			for (var i = size - bytes; i < size; i++)
-			{
-				if (bytePtrA[i] != bytePtrB[i])
-					return false;
-			}
-
-			return true;
+#if UNITY_5_3_OR_NEWER
+			var size = UnsafeUtility.SizeOf<T>();
+			return UnsafeUtility.MemCmp(a.AsPointer(), b.AsPointer(), size) == 0;
+#else
+			return Unsafe.AreSame(ref a, ref b);
+#endif
 		}
 
-		public static bool IsDefault<T>(this ref T value) where T : unmanaged
+		public static bool IsDefault<T>(this ref T value) where T : struct
 		{
-			var size = sizeof(T);
-
-			var bytes = size % sizeof(int);
-			var ints = (size - bytes) / sizeof(int);
-
-			var intPtr = (int*)AsPointer(ref value);
-			for (var i = 0; i < ints; i++)
-			{
-				if (intPtr[i] != default)
-					return false;
-			}
-			var bytePtr = (byte*)intPtr;
-			for (var i = size - bytes; i < size; i++)
-			{
-				if (bytePtr[i] != default)
-					return false;
-			}
-
-			return true;
+			var defaultValue = default(T);
+#if UNITY_5_3_OR_NEWER
+			var size = UnsafeUtility.SizeOf<T>();
+			return UnsafeUtility.MemCmp(value.AsPointer(), defaultValue.AsPointer(), size) == 0;
+#else
+			return Unsafe.AreSame(ref value, ref defaultValue);
+#endif
 		}
 	}
 }
