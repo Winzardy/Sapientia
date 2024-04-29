eed55d7cb6bf9afe61b17da11e95ba6ec21e46fd
Vlod Shvetsov <nsgbcz@gmail.com>
2024-04-29 14:27:57 +0300

    СsvExt add hasHeader argument

--- Extensions/СsvExt.cs
+++ Extensions/СsvExt.cs
@@ -11,10 +11,10 @@ namespace Sapientia.Extensions
 	public static class СsvExt
 	{
 #region From
-		public static SimpleList<T> FromCsv<T>(this TextReader reader)
+		public static SimpleList<T> FromCsv<T>(this TextReader reader, bool hasHeader = true)
 		{
 			var values = new SimpleList<T>();
-			using var csvReader = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+			using var csvReader = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture){ HasHeaderRecord = hasHeader });
 			// CsvReader actually reads values only when you iterate the GetRecords IEnumerable
 			// You can not use this IEnumerable outside CsvReader using block!
 			foreach (var record in csvReader.GetRecords<T>())
@@ -22,40 +22,37 @@ namespace Sapientia.Extensions
 			return values;
 		}
 
-		public static SimpleList<T> FromCsv<T>(this string csv)
+		public static SimpleList<T> FromCsv<T>(this string csv, bool hasHeader = true)
 		{
 			using var reader = new StringReader(csv);
-			return FromCsv<T>(reader);
+			return FromCsv<T>(reader, hasHeader);
 		}
 
-		public static SimpleList<T> FromCsvFile<T>(this string filePath)
+		public static SimpleList<T> FromCsvFile<T>(this string filePath, bool hasHeader = true)
 		{
 			using var reader = new StreamReader(filePath);
-			return FromCsv<T>(reader);
+			return FromCsv<T>(reader, hasHeader);
 		}
 #endregion
 
 #region To
-		public static void ToCsv<T>(this IEnumerable<T> values, TextWriter writer)
+		public static void ToCsv<T>(this IEnumerable<T> values, TextWriter writer, bool hasHeader = true)
 		{
-			using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+			using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = hasHeader });
 			csvWriter.WriteRecords(values);
 		}
 
-		public static string ToCsv<T>(this IEnumerable<T> values)
+		public static string ToCsv<T>(this IEnumerable<T> values, bool hasHeader = true)
 		{
 			using var writer = new StringWriter();
-			using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
-			csvWriter.WriteRecords(values);
-
+			values.ToCsv(writer, hasHeader);
 			return writer.ToString();
 		}
 
-		public static void ToCsvFile<T>(this IEnumerable<T> values, string filePath)
+		public static void ToCsvFile<T>(this IEnumerable<T> values, string filePath, bool hasHeader = true)
 		{
 			using var writer = new StreamWriter(filePath);
-			using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
-			csvWriter.WriteRecords(values);
+			values.ToCsv(writer, hasHeader);
 		}
 #endregion
 	}
