7533b9fd706d1abeef8013bbdf39d11f8dce13ea
Vlad Shvetsov <vlad.shvetsov@winzardy.com>
2024-06-22 23:12:56 +0300

    SimpleList add ExpandCount methods

--- Collections/SimpleList.cs
+++ Collections/SimpleList.cs
@@ -175,6 +175,25 @@ namespace Sapientia.Collections
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ExpandCount(int count)
+		{
+			if (_count < count)
+			{
+				Expand(count);
+				_count = count;
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public void ExpandCount(int count, T defaultValue)
+		{
+			if (_count < count)
+			{
+				AddRange(defaultValue, count - _count);
+			}
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public void Swap(int indexA, int indexB)
 		{
 			(_array[indexA], _array[indexB]) = (_array[indexB], _array[indexA]);
