8a18bd7d4808bc9ae6322f9deb5f491233a83cfb
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2024-12-12 21:44:01 +0300

    fixed MessageSubscriber

--- Extensions/CompositeDisposable.cs
+++ Extensions/CompositeDisposable.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
-using Sapientia.Extensions;
 using Sapientia.Pooling;
 
-namespace Generic
+namespace Sapientia.Extensions
 {
 	public class CompositeDisposable : IDisposable
 	{
