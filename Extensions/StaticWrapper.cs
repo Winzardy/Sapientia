2bc2effa33b0a374f048a01ec6e0cb9f1e0ad5c8
Vlad Shvetsov <vlad.shvetsov@winzardy.com>
2024-12-12 20:16:18 +0300

    [TASK-1364] Remove StaticWrapper.Instance property

--- Extensions/StaticWrapper.cs
+++ Extensions/StaticWrapper.cs
@@ -10,21 +10,9 @@ namespace Sapientia.Extensions
 	/// </summary>
 	public abstract class StaticWrapper<T> where T : class
 	{
-		private static T _instance;
+		protected static T _instance;
 
 		public static bool IsInitialized => _instance != null;
-
-		protected static T Instance
-		{
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get
-			{
-				if (_instance == null)
-					throw new Exception($"Trying to access [ {typeof(T).Name} ] functions while its not initialized!");
-
-				return _instance;
-			}
-		}
 
 		public static void Initialize(T instance)
 		{
