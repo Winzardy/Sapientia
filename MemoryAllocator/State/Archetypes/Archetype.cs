562ee85a6453b920b3eeb8f2aa6f146c7f427f70
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-04-24 14:28:48 +0300

    updated ContentReference, minor updated UI debug, minor updated LocKeyAttributeDrawer

--- MemoryAllocator/State/Archetypes/Archetype.cs
+++ MemoryAllocator/State/Archetypes/Archetype.cs
@@ -14,7 +14,7 @@ namespace Sapientia.MemoryAllocator.State
 	/// <summary>
 	/// Если настройки есть, то они будут доступны через `allocator.GetService<TGlobalSettings>()`
 	/// </summary>
-	public interface IGlobalSettings : IIndexedType
+	public interface IConfigurationEntry : IIndexedType
 	{
 	}
 
