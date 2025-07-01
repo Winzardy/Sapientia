fc24fee4a18e46ff043c90824c7edb5aa4d095a7
Vladislav Shvetsov <vlad.shvetsov@winzardy.com>
2025-07-01 14:49:16 +0300

    InterfaceProxyGenerator. Add comments generation

--- MemoryAllocator/TypeIndexer/Generator/InterfaceProxyGenerator.cs
+++ MemoryAllocator/TypeIndexer/Generator/InterfaceProxyGenerator.cs
@@ -248,6 +248,7 @@ namespace Sapientia.TypeIndexer
 				sourceBuilder.AppendLine($"		{BurstAttribute}");
 				sourceBuilder.AppendLine("#endif");
 				sourceBuilder.AppendLine("#endif");
+				sourceBuilder.AppendLine($"		// Чтобы найти дальнейшие `usages` метода - выше в классе `{baseType.Name}ProxyExt` найдите `usages` методов `{methodInfo.Name}`");
 				sourceBuilder.AppendLine($"		private static {returnTypeString} {methodInfo.Name}{genericParametersString}{parametersString}");
 				sourceBuilder.AppendLine("		{");
 				sourceBuilder.AppendLine($"			ref var __source = ref {typeof(UnsafeExt).FullName}.AsRef<TSource>(executorPtr);");
