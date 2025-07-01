8b93ffc995cbaafab93d3723e18aa6b9dea36afd
Konstantin Postoev <konstantin.postoev@gmail.com>
2025-07-01 16:48:48 +0700

    Add ConcatUrl method

---
+++ Utility/UrlUtility.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+
+namespace Sapientia.Utility
+{
+	public static class UrlUtility
+	{
+		public static string ConcatUrl(params string[]? parts)
+		{
+			if (parts == null || parts.Length == 0)
+				return string.Empty;
+
+			var normalizedParts = parts
+				.Where(p => !string.IsNullOrWhiteSpace(p))
+				.Select(p => p.Trim('/'))
+				.Where(p => !string.IsNullOrEmpty(p));
+
+			return string.Join("/", normalizedParts);
+		}
+	}
+}
