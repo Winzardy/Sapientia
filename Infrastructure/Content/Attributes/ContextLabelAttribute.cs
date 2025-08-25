57b7f5ef4e2dd5e810100f3bca734d3b27330bd9
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-08-25 14:32:50 +0300

    [TASK-2088] [!] Fixed ContextLabelAttribute

--- Infrastructure/Content/Attributes/ContextLabelAttribute.cs
+++ Infrastructure/Content/Attributes/ContextLabelAttribute.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Diagnostics;
-using System.Runtime.Remoting.Contexts;
-using Fusumity.Attributes;
 
 namespace Content
 {
@@ -9,7 +7,7 @@ namespace Content
 	/// Превращает поле в человеко-читаемый лейбл в инспекторе.
 	/// Для корректной работы требуется, чтобы в контенте существовала коллекция с типом <paramref name="type"/>.
 	/// </summary>
-	[Conditional("CLIENT")]
+	[Conditional("UNITY_EDITOR")]
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
 	public class ContextLabelAttribute : Attribute
 	{
@@ -23,27 +21,5 @@ namespace Content
 		{
 			Catalog = catalog;
 		}
-	}
-
-	/// <summary>
-	/// Превращает поле в человеко-читаемый лейбл в инспекторе.
-	/// Для корректной работы требуется, чтобы в контенте существовала коллекция с типом <paramref name="type"/>.
-	/// </summary>
-	[Conditional("CLIENT")]
-	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-	public class ContextLabelParentAttribute : ParentAttribute
-	{
-		/// <summary>
-		/// Тип коллекции, из которой берутся значения для отображения.
-		/// Обычно это идентификатор или путь к коллекции (например, "Attachments", "Buffs" и т.д.)
-		/// </summary>
-		public string Catalog { get; }
-
-		public ContextLabelParentAttribute(string catalog)
-		{
-			Catalog = catalog;
-		}
-
-		public override Attribute Convert() => new ContextAttribute(Catalog);
 	}
 }
