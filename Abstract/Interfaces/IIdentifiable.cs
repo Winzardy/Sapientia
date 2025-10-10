b7a3df9f2bdd07e11ccb48ee8027d908d8746b2e
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-10-10 17:51:13 +0300

    [TASK-2265] [!] Merge #2...

--- Abstract/Interfaces/IIdentifiable.cs
+++ Abstract/Interfaces/IIdentifiable.cs
@@ -22,12 +22,4 @@ namespace Sapientia
 	{
 		public void SetId(string id);
 	}
-
-	// TODO: -> IdentifiableConfig
-	public class Identifiable : IExternallyIdentifiable
-	{
-		private string _id;
-		public string Id => _id;
-		void IExternallyIdentifiable.SetId(string id) => _id = id;
-	}
 }
