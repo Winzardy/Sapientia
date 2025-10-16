50e4ebc895d27459cc33228df828f46a583cc17d
Vladislav Shvetsov <vlad.shvetsov@winzardy.com>
2025-10-16 13:51:03 +0300

    Update LogicGraph
    # Conflicts:
    #	LogicGraph/Logic/BlueprintInstance.cs
    #	LogicGraph/Logic/LogicGraph.cs
    #	LogicGraph/Logic/StaticData/BlueprintCompiler.cs
    #	LogicGraph/Logic/StaticData/CompiledBlueprint.cs

--- Memory/ArenaAllocator.cs
+++ Memory/ArenaAllocator.cs
@@ -9,23 +9,23 @@ namespace Sapientia.Memory
 	/// Аллокатор памяти, который только выделяет память, но не освобождает её.
 	/// Он фиксированного размера и никогда не расширяется.
 	/// </summary>
-	public struct MonotonicAllocator : IDisposable
+	public struct ArenaAllocator : IDisposable
 	{
 		private int _reservedSize;
 		private SafePtr _memory;
 		private PtrOffset _rover;
 
-		public SafePtr<MonotonicAllocator> AllocatorPtr => _memory;
+		public SafePtr<ArenaAllocator> AllocatorPtr => _memory;
 
-		public static SafePtr<MonotonicAllocator> Create(int reservedSize)
+		public static SafePtr<ArenaAllocator> Create(int reservedSize)
 		{
-			var result = new MonotonicAllocator();
+			var result = new ArenaAllocator();
 
 			E.ASSERT(reservedSize > 0);
-			result._reservedSize = reservedSize + TSize<MonotonicAllocator>.size;
+			result._reservedSize = reservedSize + TSize<ArenaAllocator>.size;
 			result._memory = MemoryExt.MemAlloc(reservedSize, ClearOptions.ClearMemory);
 			result._rover = default;
-			result.MemAlloc<MonotonicAllocator>(out _) = result;
+			result.MemAlloc<ArenaAllocator>(out _) = result;
 
 			return result.AllocatorPtr;
 		}
@@ -90,7 +90,7 @@ namespace Sapientia.Memory
 			return ref (_memory + offset).Value<T>();
 		}
 
-		public void CreateRelativeOffset(ref PtrOffset<MonotonicAllocator> relativeOffset)
+		public void CreateRelativeOffset(ref PtrOffset<ArenaAllocator> relativeOffset)
 		{
 			relativeOffset = (AllocatorPtr - (SafePtr)relativeOffset.AsSafePtr());
 		}
@@ -102,9 +102,9 @@ namespace Sapientia.Memory
 			// Мы знаем, что состояние аллокатора сохранено в начале памяти, поэтому ничего больше сериализовать не нужно
 		}
 
-		public static SafePtr<MonotonicAllocator> Deserialize(ref StreamBufferReader stream)
+		public static SafePtr<ArenaAllocator> Deserialize(ref StreamBufferReader stream)
 		{
-			var allocator = new MonotonicAllocator();
+			var allocator = new ArenaAllocator();
 			allocator._reservedSize = stream.Read<int>();
 
 			stream.Read(ref allocator._memory, allocator._reservedSize);
@@ -112,6 +112,15 @@ namespace Sapientia.Memory
 
 			// Мы знаем, что состояние аллокатора сохранено в начале памяти, поэтому ничего больше десериализовать не нужно
 			return allocator.AllocatorPtr;
+		}
+
+		public void Reset()
+		{
+			var allocatorSize = TSize<ArenaAllocator>.size;
+			_rover = new PtrOffset(allocatorSize);
+
+			var sizeToReset = _reservedSize - allocatorSize;
+			MemoryExt.MemClear(_memory + _rover, sizeToReset);
 		}
 
 		public void Dispose()
@@ -125,57 +134,57 @@ namespace Sapientia.Memory
 	/// </summary>
 	public static class MonotonicAllocatorExt
 	{
-		public static ref MonotonicAllocator GetRelativeAllocator(this ref PtrOffset<MonotonicAllocator> relativeOffset)
+		public static ref ArenaAllocator GetRelativeAllocator(this ref PtrOffset<ArenaAllocator> relativeOffset)
 		{
 			return ref ((SafePtr)relativeOffset.AsSafePtr() + relativeOffset).Value();
 		}
 
-		public static PtrOffset MemAlloc(this ref PtrOffset<MonotonicAllocator> relativeOffset, int size)
+		public static PtrOffset MemAlloc(this ref PtrOffset<ArenaAllocator> relativeOffset, int size)
 		{
 			return relativeOffset.GetRelativeAllocator().MemAlloc(size);
 		}
 
-		public static PtrOffset MemAlloc(this ref PtrOffset<MonotonicAllocator> relativeOffset, int size, int count)
+		public static PtrOffset MemAlloc(this ref PtrOffset<ArenaAllocator> relativeOffset, int size, int count)
 		{
 			return relativeOffset.GetRelativeAllocator().MemAlloc(size, count);
 		}
 
-		public static PtrOffset<T> MemAlloc<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset)
+		public static PtrOffset<T> MemAlloc<T>(this ref PtrOffset<ArenaAllocator> relativeOffset)
 			where T : unmanaged
 		{
 			return relativeOffset.GetRelativeAllocator().MemAlloc<T>();
 		}
 
-		public static ref T MemAlloc<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset, out PtrOffset<T> offset)
+		public static ref T MemAlloc<T>(this ref PtrOffset<ArenaAllocator> relativeOffset, out PtrOffset<T> offset)
 			where T : unmanaged
 		{
 			return ref relativeOffset.GetRelativeAllocator().MemAlloc(out offset);
 		}
 
-		public static PtrOffset<T> MemAlloc<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset, int count)
+		public static PtrOffset<T> MemAlloc<T>(this ref PtrOffset<ArenaAllocator> relativeOffset, int count)
 			where T : unmanaged
 		{
 			return relativeOffset.GetRelativeAllocator().MemAlloc<T>(count);
 		}
 
-		public static SafePtr GetPtr(this ref PtrOffset<MonotonicAllocator> relativeOffset, PtrOffset offset)
+		public static SafePtr GetPtr(this ref PtrOffset<ArenaAllocator> relativeOffset, PtrOffset offset)
 		{
 			return relativeOffset.GetRelativeAllocator().GetPtr(offset);
 		}
 
-		public static SafePtr<T> GetPtr<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset, PtrOffset<T> offset)
+		public static SafePtr<T> GetPtr<T>(this ref PtrOffset<ArenaAllocator> relativeOffset, PtrOffset<T> offset)
 			where T : unmanaged
 		{
 			return relativeOffset.GetRelativeAllocator().GetPtr<T>(offset);
 		}
 
-		public static ref T GetRef<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset, PtrOffset<T> offset)
+		public static ref T GetRef<T>(this ref PtrOffset<ArenaAllocator> relativeOffset, PtrOffset<T> offset)
 			where T : unmanaged
 		{
 			return ref relativeOffset.GetRelativeAllocator().GetRef<T>(offset);
 		}
 
-		public static ref T GetRef<T>(this ref PtrOffset<MonotonicAllocator> relativeOffset, PtrOffset offset)
+		public static ref T GetRef<T>(this ref PtrOffset<ArenaAllocator> relativeOffset, PtrOffset offset)
 			where T : unmanaged
 		{
 			return ref relativeOffset.GetRelativeAllocator().GetRef<T>(offset);
