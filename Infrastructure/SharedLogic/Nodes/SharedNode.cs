26e024f541da08d9c4c39aa2ba9da01353f98b1b
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2025-11-11 13:06:58 +0300

    [TASK-2252] [*] Fixed AnimationSequencePulsar

--- Infrastructure/SharedLogic/Nodes/SharedNode.cs
+++ Infrastructure/SharedLogic/Nodes/SharedNode.cs
@@ -11,7 +11,7 @@ namespace SharedLogic
 		private string _id;
 
 		public virtual string Id => _id ??= GetType().Name
-		   .Remove(nameof(SharedNode));
+			.Remove(nameof(SharedNode));
 
 		internal ILogger Logger => _root.Logger;
 
@@ -42,6 +42,10 @@ namespace SharedLogic
 		void IPersistentNode<TData>.Load(in TData data) => OnLoad(in data);
 		void IPersistentNode<TData>.Save(out TData data) => OnSave(out data);
 
+		/// <summary>
+		/// OnInitialize вызывается раньше, чем OnLoad
+		/// Так же OnLoad всегда вызывается с (default) датой если ее нет
+		/// </summary>
 		protected abstract void OnLoad(in TData data);
 		protected abstract void OnSave(out TData data);
 	}
