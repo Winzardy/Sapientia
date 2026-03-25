f9456d3def87b3ce83f63c5f736cb78b7967902e
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-03-25 15:03:25 +0300

    [TASK-2217] Updated UIStoreWindow (wip)

--- Infrastructure/SharedLogic/Data/SharedModel.cs
+++ Infrastructure/SharedLogic/Data/SharedModel.cs
@@ -24,25 +24,43 @@ namespace SharedLogic
 
 	public static class SharedModelUtility
 	{
-		public static void Save<TData, TSharedModel>([CanBeNull] Dictionary<string, TSharedModel> idToModel, out TData[] data)
+		public static void Save<TData, TSharedModel>([CanBeNull] Dictionary<string, TSharedModel> idToModel, out TData[] data,
+			SharedDataPredicate<TData> predicate = null)
 			where TSharedModel : SharedModel<TData>
 		{
 			data = null;
 			if (idToModel.IsNullOrEmpty())
 				return;
 
-			data = new TData[idToModel.Count];
+			using var list = new SimpleList<TData>();
 			foreach (var (shared, i) in idToModel.Values.WithIndex())
-				shared.Save(out data[i]);
+			{
+				shared.Save(out var saveData);
+				if (predicate != null && !predicate(in saveData))
+					continue;
+				if(EqualityComparer<TData>.Default.Equals(saveData, default))
+					continue;
+				list.Add(in saveData);
+			}
+
+			data = list.ToArray();
 		}
 
 		public static void Load<TData, TSharedModel>([NotNull] Dictionary<string, TSharedModel> idToModel, in TData[] data,
 			SharedIdSelector<TData> selector)
 			where TSharedModel : SharedModel<TData>
 		{
+			var i = -1;
 			foreach (ref var dataItem in data.AsSpan())
 			{
+				i++;
 				var id = selector.Invoke(in dataItem);
+
+				if (id.IsNullOrEmpty())
+				{
+					SLDebug.LogWarning($"Empty or null id for {typeof(TData).Name} at index {i}...");
+					continue;
+				}
 
 				if (idToModel.TryGetValue(id, out var model))
 					model.Load(in dataItem);
@@ -51,4 +69,6 @@ namespace SharedLogic
 	}
 
 	public delegate string SharedIdSelector<TData>(in TData data);
+
+	public delegate bool SharedDataPredicate<TData>(in TData data);
 }
