ea95b9ebd0bc7edd2b6cc0b416325d18f231ddf8
Konstantin Postoev <konstantin.postoev@gmail.com>
2026-04-22 10:15:41 +0700

    [TASK-2656] CommandRunner: show error to user and restart game if command validation failed

--- Infrastructure/SharedLogic/Commands/ICommandRunner.cs
+++ Infrastructure/SharedLogic/Commands/ICommandRunner.cs
@@ -1,8 +1,39 @@
+using System;
+
 namespace SharedLogic
 {
 	public interface ICommandRunner
 	{
-		void Execute<T>(in T command) where T : struct, ICommand;
+		// TODO: имеет смысл переназвать метод в Schedule, Submit или что-то более подходящее
+		/// <returns>
+		/// <c>true</c> — команда прошла валидацию и успешно добавлена в буфер на исполнение<br/>
+		/// <c>false</c> — валидация провалена
+		/// </returns>
+		/// <remarks>
+		/// Команда может быть выполнена асинхронно или с задержкой.
+		/// Возврат <c>true</c> означает только факт добавления в буфер
+		/// и не отражает результат её выполнения.
+		/// <para/>
+		/// ⚠️ Не используйте возвращаемое значение для проверки успешности выполнения самой команды
+		/// </remarks>
+		bool Execute<T>(in T command) where T : struct, ICommand;
 		bool IsEmpty { get; }
+	}
+
+	public abstract class CommandRunnerDecorator : ICommandRunner
+	{
+		private readonly ICommandRunner _runner;
+
+		protected CommandRunnerDecorator(ICommandRunner runner)
+		{
+			_runner = runner;
+		}
+
+		public virtual bool Execute<T>(in T command) where T : struct, ICommand
+		{
+			return _runner.Execute(in command);
+		}
+
+		public virtual bool IsEmpty => _runner.IsEmpty;
 	}
 }
