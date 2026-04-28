8bef252b2963afcc67aea6df9877a4b1826886af
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-04-28 14:21:47 +0300

    [TASK-1148] [+] Added StoreDynamicCatalogOfferGenerationPolicy

--- Utility/Other/SerializeReferenceAttribute.cs
+++ Utility/Other/SerializeReferenceAttribute.cs
@@ -4,27 +4,27 @@ using System.Diagnostics;
 
 namespace UnityEngine
 {
-	/// Надоело везде писать UnityEngine.SerializeReference и оборачивать в дефайны
-	[Conditional("CLIENT")]
-	public class SerializeReferenceAttribute : Attribute
-	{
-	}
-
-	[Conditional("CLIENT")]
-	public class TooltipAttribute: Attribute
-	{
-		public TooltipAttribute(string tooltip)
-		{
-		}
-	}
-
-	[Conditional("CLIENT")]
-	public class SpaceAttribute: Attribute
-	{
-		public SpaceAttribute()
-		{
-		}
-	}
+	// /// Надоело везде писать UnityEngine.SerializeReference и оборачивать в дефайны
+	// [Conditional("CLIENT")]
+	// public class SerializeReferenceAttribute : Attribute
+	// {
+	// }
+	//
+	// [Conditional("CLIENT")]
+	// public class TooltipAttribute: Attribute
+	// {
+	// 	public TooltipAttribute(string tooltip)
+	// 	{
+	// 	}
+	// }
+	//
+	// [Conditional("CLIENT")]
+	// public class SpaceAttribute: Attribute
+	// {
+	// 	public SpaceAttribute()
+	// 	{
+	// 	}
+	// }
 }
 
 /// Надоело везде писать UnityEngine.SerializeReference и оборачивать в дефайны
