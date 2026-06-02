150259d1a41c4b8e6b5c256d66fbba116a9b2c18
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-06-02 14:46:40 +0300

    [!] Updated ContentValidator [*] Updated ObjectIsNullValueDrawer [+] Added NotEmptyAttribute

--- Infrastructure/Content/ContentReference.Empty.cs
+++ Infrastructure/Content/ContentReference.Empty.cs
@@ -2,27 +2,32 @@
 {
 	public partial struct ContentReference<T>
 	{
-		public readonly bool IsValid() => Has();
-
+		public readonly bool IsValid() => Exists();
 		public readonly bool IsEmpty() => guid == SerializableGuid.Empty;
+		private readonly bool Exists()
+		{
+			if (IsEmpty())
+				return false;
 
-		private readonly bool Has() => !IsEmpty() &&
-			guid == IContentReference.SINGLE_GUID
-				? ContentManager.Contains<T>()
-				: ContentManager.Contains<T>(in guid);
+			return guid == IContentReference.SINGLE_GUID
+					? ContentManager.Contains<T>()
+					: ContentManager.Contains<T>(in guid);
+		}
 	}
 
 	public partial struct ContentReference
 	{
-		public readonly bool Contains() => throw ContentDebug.Exception(
-			"IsEmpty can only be accessed on ContentReference<T>, use IsEmpty<T>");
-
+		public readonly bool Contains() => throw ContentDebug.Exception("IsEmpty can only be accessed on ContentReference<T>, use IsEmpty<T>");
 		public readonly bool IsEmpty() => guid == SerializableGuid.Empty;
 		public readonly bool IsValid() => true;
+		public readonly bool Exists<T>()
+		{
+			if (IsEmpty())
+				return false;
 
-		public readonly bool Contains<T>() => !IsEmpty() &&
-			guid == IContentReference.SINGLE_GUID
-				? ContentManager.Contains<T>()
-				: ContentManager.Contains<T>(in guid);
+			return guid == IContentReference.SINGLE_GUID
+					? ContentManager.Contains<T>()
+					: ContentManager.Contains<T>(in guid);
+		}
 	}
 }
