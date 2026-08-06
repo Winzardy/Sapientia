b31828c0f131667d04e0c1d761db45cb431f681a
Matvei Butovskii <matvei.butovskiy@winzardy.com>
2026-08-06 13:56:03 +0300

    [TASK-2941] [*] Regenerate interface proxies

--- MemoryAllocator/_scripts.generated/InterfaceProxyGenerator/IEnabledSubscriberProxy.generated.cs
+++ MemoryAllocator/_scripts.generated/InterfaceProxyGenerator/IEnabledSubscriberProxy.generated.cs
@@ -32,20 +32,11 @@ namespace Sapientia.TypeIndexer
 			__method.Invoke(__executorPtr, worldState, callbackReceiver);
 		}
 
-		public delegate void ProxyDisposeDelegate(void* __executorPtr, Sapientia.MemoryAllocator.WorldState worldState);
-		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
-		public readonly void ProxyDispose(void* __executorPtr, Sapientia.MemoryAllocator.WorldState worldState)
-		{
-			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 1);
-			var __method = UnsafeExt.As<Delegate, ProxyDisposeDelegate>(__delegate);
-			__method.Invoke(__executorPtr, worldState);
-		}
-
 		public delegate Sapientia.MemoryAllocator.IndexedPtr CopyDelegate(void* __executorPtr, Sapientia.MemoryAllocator.WorldState oldWS, Sapientia.MemoryAllocator.WorldState newWS, in Sapientia.MemoryAllocator.State.EntityCopyMap map);
 		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
 		public readonly Sapientia.MemoryAllocator.IndexedPtr Copy(void* __executorPtr, Sapientia.MemoryAllocator.WorldState oldWS, Sapientia.MemoryAllocator.WorldState newWS, in Sapientia.MemoryAllocator.State.EntityCopyMap map)
 		{
-			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 2);
+			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 1);
 			var __method = UnsafeExt.As<Delegate, CopyDelegate>(__delegate);
 			return __method.Invoke(__executorPtr, oldWS, newWS, in map);
 		}
@@ -54,9 +45,18 @@ namespace Sapientia.TypeIndexer
 		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
 		public readonly void AppendEntities(void* __executorPtr, Sapientia.MemoryAllocator.WorldState world, ref Sapientia.Collections.UnsafeList<Sapientia.MemoryAllocator.State.Entity> entities)
 		{
-			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 3);
+			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 2);
 			var __method = UnsafeExt.As<Delegate, AppendEntitiesDelegate>(__delegate);
 			__method.Invoke(__executorPtr, world, ref entities);
+		}
+
+		public delegate void ProxyDisposeDelegate(void* __executorPtr, Sapientia.MemoryAllocator.WorldState worldState);
+		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
+		public readonly void ProxyDispose(void* __executorPtr, Sapientia.MemoryAllocator.WorldState worldState)
+		{
+			var __delegate = IndexedTypes.GetDelegate(this._firstDelegateIndex + 3);
+			var __method = UnsafeExt.As<Delegate, ProxyDisposeDelegate>(__delegate);
+			__method.Invoke(__executorPtr, worldState);
 		}
 
 	}
@@ -81,27 +81,6 @@ namespace Sapientia.TypeIndexer
 			foreach (ref var __proxyPtr in __proxyEvent.GetEnumerable(__worldState))
 			{
 				__proxyPtr.proxy.OnEntityEnabled(__proxyPtr.GetPtr(__worldState).ptr, worldState, callbackReceiver);
-			}
-		}
-
-		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
-		public static void ProxyDispose(this in UnsafeProxyPtr<IEnabledSubscriberProxy> __proxyPtr, Sapientia.MemoryAllocator.WorldState worldState)
-		{
-			__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr().ptr, worldState);
-		}
-
-		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
-		public static void ProxyDispose(this ref ProxyPtr<IEnabledSubscriberProxy> __proxyPtr, Sapientia.MemoryAllocator.WorldState __worldState, Sapientia.MemoryAllocator.WorldState worldState)
-		{
-			__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr(__worldState).ptr, worldState);
-		}
-
-		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
-		public static void ProxyDispose(this ref ProxyEvent<IEnabledSubscriberProxy> __proxyEvent, Sapientia.MemoryAllocator.WorldState __worldState, Sapientia.MemoryAllocator.WorldState worldState)
-		{
-			foreach (ref var __proxyPtr in __proxyEvent.GetEnumerable(__worldState))
-			{
-				__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr(__worldState).ptr, worldState);
 			}
 		}
 
@@ -149,6 +128,27 @@ namespace Sapientia.TypeIndexer
 			}
 		}
 
+		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
+		public static void ProxyDispose(this in UnsafeProxyPtr<IEnabledSubscriberProxy> __proxyPtr, Sapientia.MemoryAllocator.WorldState worldState)
+		{
+			__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr().ptr, worldState);
+		}
+
+		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
+		public static void ProxyDispose(this ref ProxyPtr<IEnabledSubscriberProxy> __proxyPtr, Sapientia.MemoryAllocator.WorldState __worldState, Sapientia.MemoryAllocator.WorldState worldState)
+		{
+			__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr(__worldState).ptr, worldState);
+		}
+
+		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
+		public static void ProxyDispose(this ref ProxyEvent<IEnabledSubscriberProxy> __proxyEvent, Sapientia.MemoryAllocator.WorldState __worldState, Sapientia.MemoryAllocator.WorldState worldState)
+		{
+			foreach (ref var __proxyPtr in __proxyEvent.GetEnumerable(__worldState))
+			{
+				__proxyPtr.proxy.ProxyDispose(__proxyPtr.GetPtr(__worldState).ptr, worldState);
+			}
+		}
+
 	}
 
 	public unsafe struct IEnabledSubscriberProxy<TSource> where TSource: struct, Sapientia.MemoryAllocator.State.IEnabledSubscriber
@@ -173,27 +173,6 @@ namespace Sapientia.TypeIndexer
 		public static Delegate CreateOnEntityEnabledDelegate()
 		{
 			return new IEnabledSubscriberProxy.OnEntityEnabledDelegate(OnEntityEnabled);
-		}
-#if UNITY_5_3_OR_NEWER
-		[UnityEngine.Scripting.Preserve]
-#endif
-		// Чтобы найти дальнейшие `usages` метода - выше в классе `IEnabledSubscriberProxyExt` найдите `usages` методов `ProxyDispose`
-		private static void ProxyDispose(void* executorPtr, Sapientia.MemoryAllocator.WorldState worldState)
-		{
-			ref var __source = ref Sapientia.Extensions.UnsafeExt.AsRef<TSource>(executorPtr);
-#if PROXY_REFACTORING
-#else
-			__source.ProxyDispose(worldState);
-#endif
-		}
-
-#if UNITY_5_3_OR_NEWER
-		[UnityEngine.Scripting.Preserve]
-#endif
-		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
-		public static Delegate CreateProxyDisposeDelegate()
-		{
-			return new IEnabledSubscriberProxy.ProxyDisposeDelegate(ProxyDispose);
 		}
 #if UNITY_5_3_OR_NEWER
 		[UnityEngine.Scripting.Preserve]
@@ -237,6 +216,27 @@ return default;
 		public static Delegate CreateAppendEntitiesDelegate()
 		{
 			return new IEnabledSubscriberProxy.AppendEntitiesDelegate(AppendEntities);
+		}
+#if UNITY_5_3_OR_NEWER
+		[UnityEngine.Scripting.Preserve]
+#endif
+		// Чтобы найти дальнейшие `usages` метода - выше в классе `IEnabledSubscriberProxyExt` найдите `usages` методов `ProxyDispose`
+		private static void ProxyDispose(void* executorPtr, Sapientia.MemoryAllocator.WorldState worldState)
+		{
+			ref var __source = ref Sapientia.Extensions.UnsafeExt.AsRef<TSource>(executorPtr);
+#if PROXY_REFACTORING
+#else
+			__source.ProxyDispose(worldState);
+#endif
+		}
+
+#if UNITY_5_3_OR_NEWER
+		[UnityEngine.Scripting.Preserve]
+#endif
+		[System.Runtime.CompilerServices.MethodImplAttribute(256)]
+		public static Delegate CreateProxyDisposeDelegate()
+		{
+			return new IEnabledSubscriberProxy.ProxyDisposeDelegate(ProxyDispose);
 		}
 	}
 }
