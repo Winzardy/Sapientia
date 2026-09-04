08f12f5d118c2fb2dfe7ea9f13f45282c1225fc6
Vladislav Evdokimov <vladislav.evdokimov@winzardy.com>
2026-09-04 16:36:36 +0300

    [TASK-3054] [*] Updated "Trade cost reset" - tradeboard gets a scoped bind, so several trades can go through one board - reset walks the whole cost/reward tree instead of the first level only

--- Infrastructure/Trading/Other/ResettableTradeUtility.cs
+++ Infrastructure/Trading/Other/ResettableTradeUtility.cs
@@ -4,7 +4,8 @@ namespace Trading
 	{
 		public static void Reset(this TradeCost cost, Tradeboard tradeboard)
 		{
-			foreach (var x in cost)
+			// Перебор самой цены раскрывает только один уровень, а сбросить надо всю вложенность
+			foreach (var x in cost.EnumerateAll())
 			{
 				if (x is ITradeResettable resettableCost)
 					resettableCost.Reset(tradeboard);
@@ -13,7 +14,7 @@ namespace Trading
 
 		public static void Reset(this TradeReward reward, Tradeboard tradeboard)
 		{
-			foreach (var x in reward)
+			foreach (var x in reward.EnumerateAll())
 			{
 				if (x is ITradeResettable resettableCost)
 					resettableCost.Reset(tradeboard);
